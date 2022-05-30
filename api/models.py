b6ffe93d64f5437bf4e7e2ff977e7e02330c150d
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-05-30 17:00:25 +0800

    initial models.py with FamilyMember and Household

--- api/models.py
+++ api/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+"""
+good to have but not currently implemented:
+UUIDs: id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+"""
 
-# Create your models here.
+
+class Household(models.Model):
+
+    class HousingType(models.TextChoices):
+        condo = "Condominium", "Condominium"
+        hdb = "HDB", "HDB"
+        landed = "Landed", "Landed"
+
+    housing_type = models.CharField(
+        max_length=11, choices=HousingType.choices, default=HousingType.hdb)
+
+    def __str__(self):
+        return (
+            "Household ID: %s, Housing Type: %s" % (
+                str(self.id), self.get_housing_type_display())
+        )
+
+
+class FamilyMember(models.Model):
+    household = models.ForeignKey(
+        Household, on_delete=models.CASCADE, related_name="familymembers",)
+
+    name = models.CharField(max_length=100)
+
+    class Gender(models.TextChoices):
+        male = "Male", "Male"
+        female = "Female", "Female"
+
+    gender = models.CharField(
+        max_length=6, choices=Gender.choices, default=Gender.female)
+
+    class MaritalStatus(models.TextChoices):
+        married = "Married", "Married"
+        single = "Single", "Single"
+
+    """
+    Spouse and marital status assumptions: 
+        - If FamilyMember has spouse, marital_status must be "Married"
+        - Spouses can live separately in different households
+        - If FamilyMember's spouse is deleted (maybe they died?), marital_status should revert to "Single"
+        - Only a pair of husband and wife can be Married. All else will default to "Single" when FamilyMember is created
+    """
+
+    marital_status = models.CharField(
+        max_length=7, choices=MaritalStatus.choices, default=MaritalStatus.single)
+
+    spouse = models.OneToOneField(
+        "self", blank=True, null=True, on_delete=models.CASCADE)
+
+    class OccupationType(models.TextChoices):
+        unemployed = "Unemployed", "Unemployed"
+        student = "Student", "Student"
+        employed = "Employed", "Employed"
+
+    occupation_type = models.CharField(
+        max_length=10, choices=OccupationType.choices, default=OccupationType.employed)
+
+    """
+    Annual income assumptions: 
+        - Cannot be less than 0: follows that household income lower bound is 0
+    """
+    annual_income = models.PositiveIntegerField()
+
+    date_of_birth = models.DateField()
+
+    """ 
+    TRY OUT making one to one relationships symmetrical with modified .save()
+        - follows https://stackoverflow.com/questions/50355697/i-have-a-onetoone-relationship-between-two-objects-of-the-same-class-in-a-django
+        - hack due to issue as old as time: https://code.djangoproject.com/ticket/7689
+    """
+
+    def __str__(self):
+        return "Name: %s, ID: %s, Household: %s" % (self.name, str(self.id), str(self.household.id))
