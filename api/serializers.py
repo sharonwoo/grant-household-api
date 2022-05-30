f4364c38ecb6e254de16dc7e16e020a6b09a2822
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-05-30 17:27:33 +0800

    add rest of files so that admin can CRUD entries

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Household, FamilyMember
+
+
+class FamilyMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FamilyMember
+        fields = [
+            "id",
+            "household",
+            "name",
+            "gender",
+            "marital_status",
+            "spouse",
+            "occupation_type",
+            "annual_income",
+            "date_of_birth",
+        ]
+
+
+class HouseholdSerializer(serializers.ModelSerializer):
+    familymember = FamilyMemberSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Household
+        fields = [
+            "id",
+            "housing_type",
+            "familymembers",
+        ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.db.models import OuterRef, Subquery, DateTimeField, F, Sum
 
-# Create your views here.
+
+from .models import Household, FamilyMember
+from .serializers import HouseholdSerializer, FamilyMemberSerializer
+
+from datetime import date, timedelta
+
+# rest_framework imports
+from rest_framework import serializers, viewsets
+from rest_framework.generics import ListAPIView
+
+
+class HouseholdViewSet(viewsets.ModelViewSet):
+    serializer_class = HouseholdSerializer
+    queryset = Household.objects.all()
+
+
+class FamilyMemberViewSet(viewsets.ModelViewSet):
+    serializer_class = FamilyMemberSerializer
+    queryset = FamilyMember.objects.all()
