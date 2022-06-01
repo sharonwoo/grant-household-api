f72e3eb81ba999d6c998cff142c8fddb527db685
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-06-01 15:24:32 +0800

    added initial tests, fixed typo

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -4,7 +4,8 @@ import base64
 import pytest
 
 
-def test_base64():
+def test_base64():  # check if pytest is working as expected
+
     given = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     expected = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     assert base64.b64encode(bytes.fromhex(given)) == expected
@@ -28,7 +29,7 @@ def test_add_household_invalid_json(client):
 
 @pytest.mark.django_db
 def test_add_family_member_invalid_json(client):
-    family_members = Household.objects.all()
+    family_members = FamilyMember.objects.all()
     old_family_members_len = len(family_members)
 
     resp = client.post(
@@ -38,5 +39,5 @@ def test_add_family_member_invalid_json(client):
     )
     assert resp.status_code == 400
 
-    family_members = Household.objects.all()
+    family_members = FamilyMember.objects.all()
     assert len(family_members) == old_family_members_len
