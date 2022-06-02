c66d6425d5da1924b0b5b8bd542564f91d631b9d
Sharon Woo <sharon.woo.2017@mitb.smu.edu.sg>
2022-06-02 15:43:32 +0800

    orz

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -11,6 +11,11 @@ def test_base64():  # check if pytest is working as expected
     assert base64.b64encode(bytes.fromhex(given)) == expected
 
 
+"""
+basic tests to check if invalid objects are not accepted. 
+"""
+
+
 @pytest.mark.django_db
 def test_add_household_invalid_json(client):
     households = Household.objects.all()
@@ -41,3 +46,41 @@ def test_add_family_member_invalid_json(client):
 
     family_members = FamilyMember.objects.all()
     assert len(family_members) == old_family_members_len
+
+
+"""
+basic model tests
+"""
+
+
+@pytest.mark.django_db
+def test_household_model():
+    household = Household(housing_type="HDB")
+    household.save()
+    assert household.housing_type == "HDB"
+    assert household.uuid
+    assert household.created_date
+    assert household.updated_date
+    assert household.family_members
+
+
+@pytest.mark.django_db
+def test_family_member_model():
+    # household = Household(housing_type="HDB")
+    # household.save()
+
+    family_member = FamilyMember(household="9f5609e5-ce08-4107-9750-71a704607861",
+                                 name="pytest person 1",
+                                 gender="Male",
+                                 marital_status="Single",
+                                 spouse=None,
+                                 occupation_type="Unemployed",
+                                 annual_income=0,
+                                 date_of_birth="1987-06-02"
+                                 )
+    family_member.save()
+
+    assert family_member.uuid
+    assert family_member.created_date
+    assert family_member.updated_date
+    assert family_member.name == "pytest person 1"
